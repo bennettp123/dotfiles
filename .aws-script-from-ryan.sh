981dee9187ca43abefbc04c72bcd09b60e9d5b24
Bennett Perkins <1610227+bennettp123@users.noreply.github.com>
2024-02-06 13:30:38 +0800

    rename aws script

--- .aws-script-from-ryan.sh
+++
@@ -1,7 +0,0 @@
-
-aws-list-instances ()
-{
-    aws ec2 describe-instances $@ \
-     | jq '.Reservations[]?.Instances[]? | .InstanceId + ", " + .InstanceType + ", " + .PrivateIpAddress + ", " + .PrivateDnsName + ", " + (.Tags[]? // [] | select(.Key?=="Name") | .Value) + ", " + .PublicIpAddress'
-}
-
