e10bd1d1621f82f9634eb66d9bee54380f985a38
Bennett Perkins <1610227+bennettp123@users.noreply.github.com>
2024-02-07 08:30:39 +0800

    commit new dotfiles

--- .oh-my-zsh/custom/plugins/bennetts-dotfiles/bennetts-dotfiles.plugin.zsh
+++ .oh-my-zsh/custom/plugins/bennetts-dotfiles/bennetts-dotfiles.plugin.zsh
@@ -1,37 +1,84 @@
 #!/bin/zsh
 
-SOURCE_DIR_RAW="${HOME}/src/dotfiles"
-SOURCE_DIR="$( cd "${SOURCE_DIR_RAW}" && echo "${PWD}" )"
-
-dotfiles_are_committed() {
-  ( cd "${SOURCE_DIR}" && test -z "$(git status --porcelain)" )
+commit_and_push_dotfiles() {
+  (
+    set -e -o pipefail
+    load_dotfiles_settings_dfa259
+    cd "${DOTFILES_SOURCE}"
+    git add .
+    git commit
+    git push
+  )
 }
 
-changes_are_pushed() {
-  ( cd "${SOURCE_DIR}" && test -z "$(git log HEAD --not --remotes)" )
+install_dotfiles() {
+  (
+    set -e -o pipefail
+    load_dotfiles_settings_dfa259
+    stow "${DOTFILES_SOURCE}" -t "${DOTFILES_TARGET}"
+  )
 }
 
-DOTFILE_WARNING="${fg[red]}$(<<EOF
-warning${reset_color}: there are uncommitted dotfiles at ${SOURCE_DIR}!
-to suppress this warning, commit all modified dotfiles, or
-run the commands below:
-  ${fg_no_bold[white]}
-  ( cd "${SOURCE_DIR}" && git commit . && git push )
-  ${reset_color}
-EOF
-)"
-
-warn_about_uncommitted_dotfiles() {
-  if ! ( dotfiles_are_committed && changes_are_pushed ); then
-    echo
-    echo "${DOTFILE_WARNING}" | sed 's/^/  /'
-    echo
+load_dotfiles_settings_dfa259() {
+  if test -r "${BENNETTS_DOTFILES_CONFIG:-"${HOME}/.bennetts-dotfiles.config"}"; then
+    source "${BENNETTS_DOTFILES_CONFIG:-"${HOME}/.bennetts-dotfiles.config"}"
   fi
+  DOTFILES_SOURCE="${DOTFILES_SOURCE:-"${HOME}/.dotfiles"}"
+  DOTFILES_TARGET="${DOTFILES_TARGET:-"${HOME}"}"
+}
+
+dotfiles_are_committed_867a65() {
+  (
+    set -e -o pipefail
+    load_dotfiles_settings_dfa259
+    cd "${DOTFILES_SOURCE}"
+    test -z "$(git status --porcelain)"
+  )
+}
+
+changes_are_pushed_d2faff() {
+  (
+    set -e -o pipefail
+    load_dotfiles_settings_dfa259
+    cd "${DOTFILES_SOURCE}"
+    test -z "$(git log HEAD --not --remotes)"
+  )
+}
+
+warn_about_uncommitted_dotfiles_dc75e1() {
+  (
+    set -e -o pipefail
+    load_dotfiles_settings_dfa259
+    test -d "${DOTFILES_SOURCE}" || exit 0
+    WARNING="$(<<EOF_7f55d7
+      ${fg[red]}
+      warning${reset_color}: there are uncommitted dotfiles
+      in ${DOTFILES_SOURCE}! to suppress this warning, commit
+      and push all modified dotfiles, or run the command below:
+
+      ${fg_no_bold[white]}
+        commit_and_push_dotfiles
+      ${reset_color}
+EOF_7f55d7
+    )"
+    if ! ( dotfiles_are_committed_867a65 && changes_are_pushed_d2faff ); then
+      echo
+      echo "  ${fg_no_bold[white]}----------${reset_color}"
+      echo "${WARNING}" | sed 's/^  //'
+      echo
+      echo "  ${fg_no_bold[white]}----------${reset_color}"
+      echo
+    fi
+  )
 }
 
 if (( $precmd_functions[(Ie)warn_about_uncommitted_dotfiles] )); then
   precmd_functions[$precmd_functions[(Ie)warn_about_uncommitted_dotfiles]]=()
 fi
 
-precmd_functions+=(warn_about_uncommitted_dotfiles)
+if (( $precmd_functions[(Ie)warn_about_uncommitted_dotfiles_dc75e1] )); then
+  precmd_functions[$precmd_functions[(Ie)warn_about_uncommitted_dotfiles_dc75e1]]=()
+fi
+
+precmd_functions+=(warn_about_uncommitted_dotfiles_dc75e1)
 
