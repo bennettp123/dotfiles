8e9841a169b351f90f3d35af2e39e0873d066f38
Bennett Perkins <1610227+bennettp123@users.noreply.github.com>
2024-04-16 20:17:22 +0800

    add more power setting overrides

--- system/usr/local/bin/apply-power-settings.sh
+++ system/usr/local/bin/apply-power-settings.sh
@@ -13,6 +13,18 @@
 # put the system to sleep after 15 minute(s) when on charger (-c)
 pmset -c sleep 15
 
-# put the display to sleep after 1 minute(s) when on charger (-c)
-pmset -c displaysleep 1
+# put the system to sleep after 10 minute(s) when on battery (-b)
+pmset -b sleep 10
+
+# put the display to sleep after 5 minute(s) when on charger (-c)
+pmset -c displaysleep 5
+
+# put the display to sleep after 1 minute(s) when on battery (-b)
+pmset -b displaysleep 1
+
+# disable wake-on-magic-packet when on battery (-b)
+pmset -b womp 0
+
+# enable wake-on-magic-packet when on charger (-c)
+pmset -c womp 1
 
