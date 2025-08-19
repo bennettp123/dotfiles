a01a70cc2c85d48605abc1c18d4bd35354f9bf91
Bennett Perkins <1610227+bennettp123@users.noreply.github.com>
2025-08-19 11:13:46 +0800

    improve system install script

--- system/install.sh
+++ system/install.sh
@@ -5,29 +5,46 @@ set -e -o pipefail
 SYSTEM_SOURCE_DIR="${SOURCE_DIR:-$(dirname "$0")}"
 SYSTEM_TARGET_DIR="${SYSTEM_TARGET_DIR}"
 
+cd "${SYSTEM_SOURCE_DIR}" || exit 1
+
 if [ -z "${SYSTEM_TARGET_DIR}" ] || [ "${SYSTEM_TARGET_DIR}" == "/" ]; then
   xattr -d -r -v com.apple.provenance "${SYSTEM_SOURCE_DIR}"
-  if launchctl bootout system "${SYSTEM_TARGET_DIR}/Library/LaunchDaemons/com.bennettp123.fix-chimpy-power-settings.plist"; then
-    echo 'removed existing launchd service'
-  fi
 fi
 
-rm -fv "${SYSTEM_TARGET_DIR}/Library/LaunchDaemons/com.bennettp123.fix-chimpy-power-settings.plist"
 rm -fv "${SYSTEM_TARGET_DIR}/usr/local/bin/apply-power-settings.sh"
-
-cp -fv "${SYSTEM_SOURCE_DIR}/Library/LaunchDaemons/com.bennettp123.fix-chimpy-power-settings.plist" "${SYSTEM_TARGET_DIR}/Library/LaunchDaemons/com.bennettp123.fix-chimpy-power-settings.plist"
 cp -fv "${SYSTEM_SOURCE_DIR}/usr/local/bin/apply-power-settings.sh" "${SYSTEM_TARGET_DIR}/usr/local/bin/apply-power-settings.sh"
-
-chown root:wheel "${SYSTEM_TARGET_DIR}/Library/LaunchDaemons/com.bennettp123.fix-chimpy-power-settings.plist"
-chmod 644 "${SYSTEM_TARGET_DIR}/Library/LaunchDaemons/com.bennettp123.fix-chimpy-power-settings.plist"
-
 chown root:wheel "${SYSTEM_TARGET_DIR}/usr/local/bin/apply-power-settings.sh"
 chmod 755 "${SYSTEM_TARGET_DIR}/usr/local/bin/apply-power-settings.sh"
 
-if [ -z "${SYSTEM_TARGET_DIR}" ] || [ "${SYSTEM_TARGET_DIR}" == "/" ]; then
-  if ! launchctl bootstrap system "${SYSTEM_TARGET_DIR}/Library/LaunchDaemons/com.bennettp123.fix-chimpy-power-settings.plist"; then
-    echo 'failed to install launchd service'
-    exit 1
+for file in Library/LaunchDaemons/*.plist Library/LaunchAgents/*.plist; do
+
+  if ! [ -r "$file" ]; then continue; fi
+
+  rm -fv "${SYSTEM_TARGET_DIR}/${file}"
+  cp -fv "${SYSTEM_SOURCE_DIR}/${file}" "${SYSTEM_TARGET_DIR}/${file}"
+
+  chown root:wheel "${SYSTEM_TARGET_DIR}/${file}"
+  chmod 644 "${SYSTEM_TARGET_DIR}/${file}"
+
+  if [ -z "${SYSTEM_TARGET_DIR}" ] || [ "${SYSTEM_TARGET_DIR}" == "/" ]; then
+    if grep -q LaunchDaemons <<<"$(dirname "${file}")" ; then
+      launchctl bootout system "${SYSTEM_TARGET_DIR}/${file}" || :
+      if ! launchctl bootstrap system "${SYSTEM_TARGET_DIR}/${file}"; then
+	echo "failed to bootstrap launch daemon ${file}"
+	exit 1
+      fi
+    else
+      LABEL="$(defaults read "${SYSTEM_TARGET_DIR}/${file}" Label 2>/dev/null || echo "")"
+      if [ -z "${LABEL}" ]; then
+	LABEL=$(basename "${file}" .plist)
+      fi
+
+      if ! launchctl enable system/"${LABEL}" && launchctl start system/"${LABEL}"; then
+	echo "failed to enable launch agent ${file}"
+	exit 1
+      fi
+    fi
   fi
-fi
+
+done
 
